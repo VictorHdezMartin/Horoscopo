7e83c7eb7d852d26011dd2ee98b6486cd8426579
bermonch <doblado007@gmail.com>
2024-10-29 20:59:00 +0100

    29 de octubre de 2024 20:58  v.1.0.1

--- app/src/main/java/com/example/horoscopo/activities/ListActivity.kt
+++ app/src/main/java/com/example/horoscopo/activities/ListActivity.kt
@@ -2,20 +2,34 @@ package com.example.horoscopo.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.horoscopo.Data.Horoscope
 import com.example.horoscopo.Data.HoroscopeProvider
 import com.example.horoscopo.R
+import com.example.horoscopo.adapters.HoroscopeAdapter
+import com.google.android.material.divider.MaterialDivider
+import com.google.android.material.divider.MaterialDividerItemDecoration
 
 class ListActivity : AppCompatActivity() {
 
     lateinit var horoscopeList: List<Horoscope>
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_list)
 
+        recyclerView = findViewById(R.id.recyclerView)
         horoscopeList = HoroscopeProvider.findAll()
 
-    }
+        val adapter = HoroscopeAdapter(horoscopeList)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+
+        val divider = MaterialDividerItemDecoration(this, LinearLayoutManager.VERTICAL)
+        recyclerView.addItemDecoration(divider)
+      }
 }
---
+++ app/src/main/java/com/example/horoscopo/adapters/adapters.kt
@@ -0,0 +1,47 @@
+package com.example.horoscopo.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.horoscopo.Data.Horoscope
+import com.example.horoscopo.R
+
+class HoroscopeAdapter (val items: List<Horoscope>): RecyclerView.Adapter<HoroscopeViewHolder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HoroscopeViewHolder {
+
+       val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_horoscope, parent, false)
+        return HoroscopeViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+       return items.size
+    }
+
+    override fun onBindViewHolder(holder: HoroscopeViewHolder, position: Int) {
+       val horoscope = items[position]
+       holder.render(horoscope)
+    }
+}
+
+class HoroscopeViewHolder(view: View): RecyclerView.ViewHolder (view){
+
+    var nameTextView: TextView = view.findViewById(R.id.nameTextView)
+    var dateTextView: TextView = view.findViewById(R.id.dateTextView)
+    var symbolImageView: ImageView = view.findViewById(R.id.symbolImageView)
+
+    fun render(horoscope: Horoscope){
+
+      // llamada fuera del activity para poder acceder a los metodos
+      //  var context = itemView.context
+      //  nameTextView.text = context.getString(horoscope.name)
+      //  symbolImageView.setImageDrawable((context.getDrawable(horoscope.image)))
+
+        nameTextView.setText(horoscope.name)
+        dateTextView.setText(horoscope.dates)
+        symbolImageView.setImageResource(horoscope.image)
+    }
+}
